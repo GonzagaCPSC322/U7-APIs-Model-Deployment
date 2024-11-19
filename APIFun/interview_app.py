c623ddd3bf4cee5a88cb27fd3a91ddcb91477d7f
Gina Sprint <sprint@gonzaga.edu>
2024-11-19 09:37:51 -0800

    finished flask app

--- APIFun/interview_app.py
+++ APIFun/interview_app.py
@@ -1,4 +1,8 @@
 import pickle
+# we are going to use the Flask micro web framework
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 def load_model():
     # unpickle header and tree in tree.p
@@ -17,7 +21,39 @@ def tdidt_predict(header, tree, instance):
         if value_list[1] == instance[att_index]:
             return tdidt_predict(header, value_list[2], instance)
         
-if __name__ == "__main__":
+# we need to add some routes!
+# a "route" is a function that handles a request
+# e.g. for the HTML content for a home page
+# or for the JSON response for a /predict API endpoint, etc
+@app.route("/")
+def index():
+    # return content and status code
+    return "<h1>Welcome to the interview predictor app</h1>", 200
+        
+
+# lets add a route for the /predict endpoint
+@app.route("/predict")
+def predict():
+    # lets parse the unseen instance values from the query string
+    # they are in the request object
+    level = request.args.get("level") # defaults to None
+    lang = request.args.get("lang")
+    tweets = request.args.get("tweets")
+    phd = request.args.get("phd")
+    instance = [level, lang, tweets, phd]
     header, tree = load_model()
-    print(header)
-    print(tree)
+    # lets make a prediction!
+    pred = tdidt_predict(header, tree, instance)
+    if pred is not None:
+        return jsonify({"prediction": pred}), 200
+    # something went wrong!!
+    return "Error making a prediction", 400
+
+
+if __name__ == "__main__":
+    # header, tree = load_model()
+    # print(header)
+    # print(tree)
+    app.run(host="0.0.0.0", port=5001, debug=False)
+    # TODO: when deploy app to "production", set debug=False
+    # and check host and port values
